5a3c1cd917b7ed48b4b6376414cc2bf48f99d68d
Pablo-Longoni <pablolongoni19@gmail.com>
2025-10-02 11:45:04 -0300

    buxFixed PanelPause, rotationInput, gm

---
+++ Assets/Scripts/UI/AssignButtons.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using UnityEngine.UI;
+public class AssignButtons : MonoBehaviour
+{
+ /*   public ChangeScene GameManager; 
+    private void Awake()
+    {
+        
+
+        if (GameManager != null)
+        {
+            Button btn = GetComponent<Button>();
+            btn.onClick.AddListener(GameManager.TogglePause);
+        }
+    }
+
+    */
+}
