c1efd2d35e23edf8af4616f1eec4afe1978b87b3
Pablo-Longoni <pablolongoni19@gmail.com>
2025-10-12 10:10:49 -0300

    portal effect, rippleSplash particle

--- Assets/Scripts/Eviroment/DeactivateCollider.cs
+++ Assets/Scripts/Eviroment/DeactivateCollider.cs
@@ -1,18 +1,31 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class DeactivateCollider : MonoBehaviour
 {
     [SerializeField] public Collider _targetCollider;
-
+    [SerializeField] public ParticleSystem _ripplePrefab;
+    [SerializeField] public ParticleSystem _waterSplashPrefab;
+    [SerializeField] public GameObject _playerInput;
     private bool _isTriggered;
+
+    private Quaternion _rotation = Quaternion.Euler(-90,0,0);
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player") && !_isTriggered)
         {
             _targetCollider.enabled = false;
             _isTriggered = true;
+
+            _playerInput.SetActive(false);
+
+            Instantiate(_ripplePrefab, _targetCollider.transform.position, Quaternion.identity);
+            Instantiate(_waterSplashPrefab, _targetCollider.transform.position, _rotation);
+
             StartCoroutine(EnabledCollider());
+
+          
             Debug.Log("Entro a la sombra");
         }
     }
@@ -30,7 +43,11 @@ public class DeactivateCollider : MonoBehaviour
     private IEnumerator EnabledCollider()
     {
         yield return new WaitForSeconds(.7f);
+
+        _playerInput.SetActive(true);
+
         _targetCollider.enabled = true;
         _isTriggered = false;
+       
     }
 }
